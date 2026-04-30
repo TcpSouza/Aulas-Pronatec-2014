348bcbe5e2ddd9d5eaacfd9885e9deb2323bc7c6
Tiago Lima <tiagolsouza86@gmail.com>
2026-04-30 14:32:19 -0300

    Add files via upload

---
+++ macros de excel semana 1/calculo equacao erivelton aula.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,c,d,num,den;
+  {
+  	printf("Escreva o valor de a  ");
+  	scanf("%d/n",&a);
+  	printf("Escreva o valor de b  ");
+  	scanf("%d/n",&b);
+  	printf("Escreva o valor de c  ");
+  	scanf("%d/n",&c);
+  	printf("Escreva o valor de d  ");
+  	scanf("%d/n",&d);
+  	printf(num=(a*d)+(b*c));
+  	printf(den=(b*d));
+  	printf("O numerador e %d",&num);
+  	printf("O denominador e %d",&den);
+  	 system"pause";
+	
+	  return(0);
+  	
+  }	
+	
+	
+	
+	
+}
